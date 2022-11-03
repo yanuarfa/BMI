30f695770c38d98d619a258764e0a9ce883b9a8e
yanuarfa <yanuarfatur41@gmail.com>
2022-11-03 21:51:37 +0700

    first commit

---
+++ app/src/main/java/b1/uts0703040054/achmadfauzan/HomeActivity.kt
@@ -0,0 +1,11 @@
+package b1.uts0703040054.achmadfauzan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
