6350539c8fd42cbd88c3bd9346dfc4c21a2e4e71
yanuarfa <yanuarfatur41@gmail.com>
2022-11-05 23:38:08 +0700

    testing code

--- app/src/main/java/b1/uts0703040054/achmadfauzan/HomeActivity.kt
+++ app/src/main/java/b1/uts0703040054/achmadfauzan/HomeActivity.kt
@@ -11,12 +11,13 @@ class HomeActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var btnHitung: Button
     private lateinit var tvHasil: TextView
 
+    var spinJenisKelamin = ""
+//    var hasilBMI = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        beratBadan = findViewById(R.id.beratBadan)
-        tinggiBadan = findViewById(R.id.tinggiBadan)
         btnHitung = findViewById(R.id.btnHitung)
         tvHasil = findViewById(R.id.tvHasil)
 
@@ -28,17 +29,38 @@ class HomeActivity : AppCompatActivity(), View.OnClickListener {
             spinner.adapter = adapter
         }
 
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                spinJenisKelamin = p0?.getItemAtPosition(p2).toString()
+            }
+        }
+
         btnHitung.setOnClickListener(this)
     }
 
     override fun onClick(p0: View?) {
+        beratBadan = findViewById(R.id.beratBadan)
+        tinggiBadan = findViewById(R.id.tinggiBadan)
         if(p0?.id == R.id.btnHitung) {
-            val inputBeratBadan = beratBadan.text.toString().trim()
-            val inputTinggiBadan = tinggiBadan.text.toString().trim()
-            val tinggiMeter = inputTinggiBadan.toDouble() / 100
-
-            val bmi = inputBeratBadan.toDouble() / (tinggiMeter.toDouble() * tinggiMeter.toDouble())
-            tvHasil.text = bmi.toString()
+            if(beratBadan.text.toString() === "") {
+                tvHasil.text = spinJenisKelamin
+            }
         }
+//        if(beratBadan.getText().toString().trim() === "") {
+//            Toast.makeText(applicationContext, "Form wajib diisi!",Toast.LENGTH_LONG).show()
+//        } else {
+//            if(p0?.id == R.id.btnHitung) {
+//                val inputBeratBadan = beratBadan.text.toString().trim()
+//                val inputTinggiBadan = tinggiBadan.text.toString().trim()
+//                val tinggiMeter = inputTinggiBadan.toDouble() / 100
+//
+//                val bmi = inputBeratBadan.toDouble() / (tinggiMeter.toDouble() * tinggiMeter.toDouble())
+//                tvHasil.text = spinJenisKelamin
+//            }
+//        }
     }
 }
